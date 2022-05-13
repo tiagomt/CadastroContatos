f4160751e6a352f2d02fd12ea3e01ab7b5f0f2b7
Tiagomt <tiago-tmt@hotmail.com>
2022-05-13 11:56:34 -0300

    Alterada a query

--- criação database/data.sql
+++ criação database/data.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 13-Maio-2022 às 16:07
+-- Tempo de geração: 13-Maio-2022 às 16:29
 -- Versão do servidor: 10.4.17-MariaDB
 -- versão do PHP: 8.0.2
 
@@ -23,51 +23,32 @@ SET time_zone = "+00:00";
 
 -- --------------------------------------------------------
 
+CREATE DATABASE data;
 --
 -- Estrutura da tabela `usuarios`
 --
 
+
 CREATE TABLE `usuarios` (
-  `id_usuario` int(11) NOT NULL,
+  `id_usuario` int NOT NULL auto_increment,
   `nome` varchar(255) NOT NULL,
   `sobrenome` varchar(255) NOT NULL,
   `cpf` varchar(14) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `email2` varchar(255) DEFAULT NULL,
   `telefone` varchar(14) DEFAULT NULL,
-  `telefone2` varchar(14) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `telefone2` varchar(14) DEFAULT NULL,
+  CONSTRAINT PK_usuarios PRIMARY KEY(usuarios)
+);
 
---
 -- Extraindo dados da tabela `usuarios`
---
+
 
 INSERT INTO `usuarios` (`id_usuario`, `nome`, `sobrenome`, `cpf`, `email`, `email2`, `telefone`, `telefone2`) VALUES
-(1, 'Tiago', 'Teixeira', '444.444.444-44', 'tiago-tmt@hotmail.com', '', '(11)99718-4853', ''),
-(48, 'aaaaaaa', 'aaaaaa', '111.111.111-11', 'aaaaaaaaaaa', '', '(11)11111-1111', ''),
-(51, 'Tiago', 'Teixeira', '111.11111111', 'tiago@gmail.com', '', '(11)11111-1111', '');
+(1, 'Tiago', 'Teixeira', '123.456.789-09', 'tiago-tmt@hotmail.com', '', '(11)99718-4853', ''),
+(2, 'aaaaaaa', 'aaaaaa', '123.456.789-09', 'aaaa@aaaaa.com', '', '(11)11111-1111', ''),
+(3, 'Tiago', 'Teixeira', '123.456.789-09', 'tiago@gmail.com', '', '(11)11111-1111', '');
 
---
--- Índices para tabelas despejadas
---
 
---
--- Índices para tabela `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id_usuario`);
-
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
