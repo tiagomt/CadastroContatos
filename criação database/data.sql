4f16a58e480d068c76f143be41540d680b97bcbb
Tiagomt <tiago-tmt@hotmail.com>
2022-05-17 11:24:41 -0300

    Alterada query criaçao banco de dados

--- criação database/data.sql
+++ criação database/data.sql
@@ -1,36 +1,31 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
+CREATE DATABASE  IF NOT EXISTS `data` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `data`;
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
 --
--- Host: 127.0.0.1
--- Tempo de geração: 13-Maio-2022 às 16:29
--- Versão do servidor: 10.4.17-MariaDB
--- versão do PHP: 8.0.2
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: data
+-- ------------------------------------------------------
+-- Server version	8.0.29
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Banco de dados: `data`
+-- Table structure for table `usuarios`
 --
 
--- --------------------------------------------------------
-
-CREATE DATABASE data;
---
--- Estrutura da tabela `usuarios`
---
-
-
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuarios` (
-  `id_usuario` int NOT NULL auto_increment,
+  `id_usuario` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) NOT NULL,
   `sobrenome` varchar(255) NOT NULL,
   `cpf` varchar(14) DEFAULT NULL,
@@ -38,17 +33,27 @@ CREATE TABLE `usuarios` (
   `email2` varchar(255) DEFAULT NULL,
   `telefone` varchar(14) DEFAULT NULL,
   `telefone2` varchar(14) DEFAULT NULL,
-  CONSTRAINT PK_usuarios PRIMARY KEY(usuarios)
-);
+  PRIMARY KEY (`id_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Extraindo dados da tabela `usuarios`
+--
+-- Dumping data for table `usuarios`
+--
 
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'Tiago','Teixeira','123.456.789-09','tiago-tmt@hotmail.com','','(11)99718-4853',''),(2,'aaaaaaa','aaaaaa','123.456.789-09','aaaa@aaaaa.com','','(11)11111-1111',''),(3,'Tiago','Teixeira','123.456.789-09','tiago@gmail.com','','(11)11111-1111','');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-INSERT INTO `usuarios` (`id_usuario`, `nome`, `sobrenome`, `cpf`, `email`, `email2`, `telefone`, `telefone2`) VALUES
-(1, 'Tiago', 'Teixeira', '123.456.789-09', 'tiago-tmt@hotmail.com', '', '(11)99718-4853', ''),
-(2, 'aaaaaaa', 'aaaaaa', '123.456.789-09', 'aaaa@aaaaa.com', '', '(11)11111-1111', ''),
-(3, 'Tiago', 'Teixeira', '123.456.789-09', 'tiago@gmail.com', '', '(11)11111-1111', '');
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-
-COMMIT;
-
+-- Dump completed on 2022-05-17 11:21:29
